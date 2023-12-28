023fd7d5ef2abd79a93e591e8f2c2fc67a6f550d
AnaghaMeshram2502 <145539350+AnaghaMeshram2502@users.noreply.github.com>
2023-12-28 16:12:51 +0530

    Add 2 nos

---
+++ Prog2.py
@@ -0,0 +1,11 @@
+##Write a Python program to do arithmetical operations addition and division.
+# Addition
+num1 = float(input("Enter the first number for addition: "))
+num2 = float(input("Enter the second number for addition: "))
+sum_result = num1 + num2
+print(f"sum: {num1} + {num2} = {sum_result}")
+
+##Enter the first number for addition: 9
+#Enter the second number for addition: 3
+#sum: 9.0 + 3.0 = 12.0
+#PS C:\Users\HP\Desktop\VSCODE\Chatbot> 
