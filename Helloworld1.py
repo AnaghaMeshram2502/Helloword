ce51ac58b8101e5fdc31c2288f37076a8002481c
AnaghaMeshram2502 <145539350+AnaghaMeshram2502@users.noreply.github.com>
2023-12-28 12:21:05 +0530

    changed to chatbot

--- Helloworld1.py
+++ Helloworld1.py
@@ -1,1 +1,22 @@
-print("Hello World")
+#question: answer
+quesans={
+    "Hi":"Hello",
+    "What is your name":"Minu",
+    "What is your age":"25 years",
+    "Where do you live":"Amravati",
+    "Which company do you work for":"Minzor",
+    "What is your short term goal":"Getting placed in an IT company",
+    "Whom do you admire the most":"My mother",
+    "Which Hollywood actor do you love":"Robert Pattinson",
+    "What do you do in your passtime":"Read and make Zentangles",
+    "Which book are you reading currently":"Mindset by Dr. Carol Dweck"
+       }
+
+while True:
+    qsn = input()
+    
+    if(qsn == "Quit"):
+        break
+        
+    else:
+        print(quesans[qsn])
